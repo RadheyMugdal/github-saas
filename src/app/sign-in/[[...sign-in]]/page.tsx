070e93d8db0205e828c7ba8640f76bad42e4629e
Radhey Mugdal <mugdalradhey@gmail.com>
2025-04-27 20:35:17 +0530

    feat: wrap SignIn and SignUp components in a centered div for improved layout

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,11 @@ import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
-  return <SignIn />;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <SignIn />
+    </div>
+  );
 };
 
 export default page;
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,11 @@ import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 const Page = () => {
-  return <SignUp />;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <SignUp />
+    </div>
+  );
 };
 
 export default Page;
